9f4b811b8e8069f63fd6155496aaa4c2e4674dd2
Celia <celia@iMac-de-Celia.local>
2021-09-12 18:34:36 +0200

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super(); //llama al método constructor dentro de la clase Component
+
+    this.state = {
+      monsters: [],
+      searchField: ''
+    };
+
+    // this.handleChange = this.handleChange.bind(this); //bind mira que esté en nuestro código y que sea definida en el punto correcto
+  }
+  
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({monsters: users}));
+  }
+
+  handleChange = e => {
+    this.setState({ searchField: e.target.value })
+  }
+
+  render() {
+    const { monsters, searchField } = this.state; //es la forma rápida para no escribir varias líneas con const monsters = this.state.monsters y lo mismo con searchField
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+      );
+    return (
+      <div className="App">
+        <h1>Monsters Rolodex</h1>
+        <SearchBox
+          placeholder='busca tu monstruo'
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMonsters}/>
+      </div>
+    );
+  }
 }
 
 export default App;
